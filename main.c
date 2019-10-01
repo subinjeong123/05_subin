6c133773ef6f3ae0273f0b997a4f06ede332062b
subinjeong123 <55428469+subinjeong123@users.noreply.github.com>
2019-10-01 15:33:57 +0900

    5week first project

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int input;
+	
+	printf("input a number :");
+	scanf("%d", &input);
+	
+	if (input>0)
+	printf("양수입니다. \n");
+	 else
+	 printf("음수입니다. \n");
+	return 0;
+}
